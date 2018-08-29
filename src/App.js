3cb8e7cf689995cab8d66be1cc0f17ede009903f
kenji <willian.hiraoka@fatec.sp.gov.br>
2018-08-28 22:06:48 -0300

    Redux Thunk in Practice

--- src/App.js
+++ src/App.js
@@ -1,31 +1,30 @@
 import React, { Component } from 'react';
 import { Provider } from 'react-redux';
-import { createStore } from 'redux';
-import { View, Text } from 'react-native';
+import { createStore, applyMiddleware } from 'redux';
+import ReduxThunk from 'redux-thunk';
 import reducers from './reducers';
 import firebase from 'firebase';
+import LoginForm from './components/LoginForm';
 
 class App extends Component {
     componentWillMount() {
         const config = {
-            apiKey: '',
-            authDomain: '',
-            databaseURL: '',
-            projectId: '',
-            storageBucket: '',
-            messagingSenderId: ''
+            apiKey: "AIzaSyAL7M-Nd6AL6MKOrTVc3kwkjGUPS7Btxe4",
+            authDomain: "manager-588ed.firebaseapp.com",
+            databaseURL: "https://manager-588ed.firebaseio.com",
+            projectId: "manager-588ed",
+            storageBucket: "manager-588ed.appspot.com",
+            messagingSenderId: "43681844202"
         }
         firebase.initializeApp(config);        
     }
 
     render() {
+        const store = createStore(reducers, {}, applyMiddleware(ReduxThunk));
+        
         return (
-            <Provider store={createStore(reducers)}>
-                <View>
-                    <Text>
-                        Hello!
-                    </Text>
-                </View>
+            <Provider store={store}>
+                <LoginForm />
             </Provider>
         );
     }
