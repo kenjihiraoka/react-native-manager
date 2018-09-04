81801958ac3a723c6a6ad4794c1279a5b012f12e
kenji <willian.hiraoka@fatec.sp.gov.br>
2018-09-04 09:12:22 -0300

    Add new comoponent ListItem and use it

--- src/actions/EmployeeActions.js
+++ src/actions/EmployeeActions.js
@@ -3,7 +3,8 @@ import { Actions } from 'react-native-router-flux';
 
 import {
     EMPLOYEE_UPDATE,
-    EMPLOYEE_CREATE
+    EMPLOYEE_CREATE,
+    EMPLOYEES_FETCH_SUCCESS
 } from './types';
 
 export const employeeUpdate = ({ prop, value }) => {
@@ -25,3 +26,14 @@ export const employeeCreate = ({ name, phone, shift }) => {
             });
     }
 }
+
+export const employeesFetch = () => {
+    const { currentUser } = firebase.auth();
+
+    return (dispatch) => {
+        firebase.database().ref(`users/${currentUser.uid}/employees`)
+            .on('value', snapshot => {
+                dispatch({ type: EMPLOYEES_FETCH_SUCCESS, payload: snapshot.val() });
+            });
+    };
+};
